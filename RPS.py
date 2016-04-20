4d9c31da4a9b50e40d2a3181117352d9abc9c1ec
Kharianne <katerinamrkackova@gmail.com>
2016-04-20 18:52:34 +0200

    adding comments to code

--- RPS.py
+++ RPS.py
@@ -1,22 +1,26 @@
 import random #importuje knihovnu pro generovani a praci s nahodnymi cisly
 
-player_choice = input("I choose:")
+player_choice = input("I choose:") #funkce input precte od uzivatele vstup
 
-
+#Funkce name_to_number prevadi text na cislo
+#podle zadanych parametru
 def name_to_number(name):
-    if name == "rock":
+    if name == "rock": #slovu rock priradi cislo 0
         number = 0
         return number
 
-    elif name == "paper":
+    elif name == "paper": #slovu paper priradi cislo 1
         number = 1
         return number
-    elif name == "scissors":
+    elif name == "scissors": #slovu scissors pripradi cislo 2
         number = 2
         return number
     else:
-        return -1
+        return -1 #vraci tuto hodnotu, pokud neni vstup validni
 
+#Funkce number_to_name prevadi cislo na text,
+#prevodni princip funguje naprosto stejnej
+#jako u predchazejici funkce
 def number_to_name(number):
     if number == 0:
         name = "rock"
@@ -30,18 +34,27 @@ def number_to_name(number):
     else:
         return "Given number is incorrect"
 
+#Samotna logika hry
+#Dochazi volani vyse nadefinovanych funkci
+#Vypsani volby hrace a pocitace
+#Samotne urceni vyherce
 def rps(player_choice):
-    print ()
-    print ("Player chooses:", player_choice)
-    player_number = name_to_number(player_choice)
+    print ()#odradkovani
+    print ("Player chooses:", player_choice) #jednoducha funkce print = zde vypise volbu hrace
+    player_number = name_to_number(player_choice)#zde volame funkci name_to_number, abychom prevedli
+                                                 #text (vstup od uzivatele) na cislo
 
-    #following if is for handling incorrect input
-    if player_number == -1:
-        print ("Incorrect input.")
-    comp_number = random.randrange(0, 3)
-    comp_choice = number_to_name(comp_number)
-    print ("Computer chooses:", comp_choice)
 
+    if player_number == -1: #tato podminka odchytava pripady nevalidniho vstupu
+        print ("Incorrect input.") #zde dojde k vypsani chybove hlasky
+
+    comp_number = random.randrange(0, 3) #funkce randrange slouzi pro generovani nahodnych cisel
+                                         #pri zadanych argumentech (0,3) generuje nahodne cisla: 0 1 2
+    comp_choice = number_to_name(comp_number)#zavolani teto funkce prevede vygenerovane cislo z
+                                             # predchoziho kroku na text
+    print ("Computer chooses:", comp_choice) #zde vypisujeme jakou moznost zvolil pocitac
+
+#Nasleduje urceni viteze na zaklade naseho algoritmu
     if comp_number == player_number:
         print ("Player and computer tie!")
     elif ((comp_number - player_number) % 3) == 1:
@@ -49,4 +62,5 @@ def rps(player_choice):
     else:
         print ("Player wins!")
 
+#Zde zavolame nasi funkci
 rps(player_choice)
